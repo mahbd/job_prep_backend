dc922d5ce60e2d9c2228a52df34fd1afbd6e128a
Mahmudul Alam <mahmudula2000@gmail.com>
2022-04-23 10:21:08 +0600

    Initialized Project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn job_prep.wsgi --log-file -
---
+++ job_prep/settings.py
@@ -0,0 +1,119 @@
+import os
+import sys
+from abc import ABC
+from pathlib import Path
+
+from djongo.base import DatabaseWrapper
+from djongo.operations import DatabaseOperations
+import django_heroku
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.getenv('SECRET_KEY', 'Nothing-Set-For-Now')
+DEBUG = False if os.getenv('DEBUG', 'True') == 'False' else True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'jp-mah.herokuapp.com']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'job_prep.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'job_prep.wsgi.application'
+
+
+# Patch MongoDB Database operations
+
+
+# noinspection PyAbstractClass
+class PatchedDatabaseOperations(DatabaseOperations):
+    def conditional_expression_supported_in_where_clause(self, database_type):
+        return False
+
+
+DatabaseWrapper.ops_class = PatchedDatabaseOperations
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'ENFORCE_SCHEMA': False,
+        'NAME': 'jp_db',
+        os.getenv('MONGO_JP', False) and
+        'CLIENT': {
+            'host': os.environ.get('MONGO_JP'),
+            'username': os.environ.get('MONGO_USERNAME'),
+            'password': os.environ.get('MONGO_PASSWORD'),
+            'authMechanism': 'SCRAM-SHA-1'
+        },
+        not os.environ.get('MONGO_JP', False) and
+        'CLIENT': {
+            'host': "mongodb://localhost:27017/jp_db"
+        }
+    }
+}
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'jp_db.sqlite3',
+        }
+    }
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Dhaka'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
