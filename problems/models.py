b0d6b84cc6a15be96820330e81884c9f2218e495
Mahmudul Alam <mahmudula2000@gmail.com>
2022-04-24 06:39:52 +0600

    Increased Decimal Field max digits limit to 5, decimal=3, fractional=2

--- problems/models.py
+++ problems/models.py
@@ -6,7 +6,7 @@ from users.models import User
 
 class Problem(models.Model):
     name = models.CharField(max_length=1023)
-    acceptance = models.DecimalField(max_digits=3, decimal_places=2)
+    acceptance = models.DecimalField(max_digits=5, decimal_places=2)
     difficulty = models.CharField(choices=DIFFICULTY_CHOICES, max_length=10)
     question_html = models.TextField()
     solution_html = models.TextField()
